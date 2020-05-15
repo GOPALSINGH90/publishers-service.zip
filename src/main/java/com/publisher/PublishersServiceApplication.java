f8d7037c9e409bc202739cbe38adacfa72ce576c
GOPALSINGH90 <gopalsinghr40@gmail.com>
2020-05-15 13:42:27 +0530

    intial commit

---
+++ src/main/java/com/publisher/PublishersServiceApplication.java
@@ -0,0 +1,13 @@
+package com.publisher;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PublishersServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PublishersServiceApplication.class, args);
+	}
+
+}
